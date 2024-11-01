aac7d6283efe053899fd06ebbc62da6d0b71f660
Tom Hennessy <tomlhennessy@outlook.com>
2024-11-01 11:36:23 +1000

    simple react app with basic routing

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
 import Stocks from './components/Stocks';
 import Movies from './components/Movies';
@@ -6,9 +8,17 @@ function App() {
   return (
     <div className='main'>
       <h1>App Component</h1>
-      <Home />
-      <Stocks />
-      <Movies />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/stocks">
+          <Stocks />
+        </Route>
+        <Route path="/movies">
+          <Movies />
+        </Route>
+      </Switch>
     </div>
   );
 }
